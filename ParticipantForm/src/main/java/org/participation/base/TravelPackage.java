c86c6ecbe3f13aa07a8cd94e8137e1eb33f06b34
WioN780 <wion.end@gmail.com>
2025-01-05 22:56:45 +0100

    TABS-18: Participation Form - finished database structure implementation

--- ParticipantForm/src/main/java/org/participation/base/TravelPackage.java
+++ ParticipantForm/src/main/java/org/participation/base/TravelPackage.java
@@ -1,11 +1,10 @@
 package org.participation.base;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Data;
+
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Entity
 @Data
@@ -13,4 +12,20 @@ public class TravelPackage {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int packageId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name = "destinationID", referencedColumnName = "destinationId", nullable = false)
+    private Destination destination;
+
+    @Column(nullable = false)
+    private LocalDate startDate;
+
+    @Column(nullable = false)
+    private LocalDate endDate;
+
+    @Column(nullable = false)
+    private BigDecimal price;
 }
